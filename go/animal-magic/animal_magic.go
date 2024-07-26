9b7536ff53439bc818b5189835a39f4c02584d25
phuc.nguyen <phuc.nguyen@ttekglobal.com>
2024-07-26 16:16:44 +0700

    Animal Magic

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -1,16 +1,26 @@
 package chance
 
+import (
+	"math/rand"
+	"time"
+)
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(20) + 1
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	rand.Seed(time.Now().UnixNano())
+	return rand.Float64() * 12.0
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(animals), func(i, j int) { animals[i], animals[j] = animals[j], animals[i] })
+	return animals
 }
